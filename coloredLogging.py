fc065709d7d34d29e86ff758f3945fbf99b7635f
Lowell Camp <50926659+camplowell@users.noreply.github.com>
2021-11-21 14:25:11 -0500

    Add printc

--- coloredLogging.py
+++ coloredLogging.py
@@ -1,21 +1,25 @@
 import logging
 
-class CustomFormatter(logging.Formatter):
+GREY = "\x1b[90;21m"
+YELLOW = "\x1b[33;21m"
+GREEN = "\x1b[32;21m"
+BLUE = "\x1b[34;21m"
+PURPLE = "\x1b[35;21m"
+CYAN = "\x1b[36;21m"
+RED = "\x1b[31;21m"
+BOLD_RED = "\x1b[31;1m"
+RESET = "\x1b[0m"
 
-    grey = "\x1b[37;21m"
-    yellow = "\x1b[33;21m"
-    green = "\x1b[32;21m"
-    red = "\x1b[31;21m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
+
+class ColorfulFormatter(logging.Formatter):
     format = "%(levelname)s: %(message)s (%(filename)s:%(lineno)d)"
 
     FORMATS = {
-        logging.DEBUG: grey + format + reset,
-        logging.INFO: reset + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset
+        logging.DEBUG: GREY + format + RESET,
+        logging.INFO: BLUE + format + RESET,
+        logging.WARNING: YELLOW + format + RESET,
+        logging.ERROR: RED + format + RESET,
+        logging.CRITICAL: BOLD_RED + format + RESET
     }
 
     def format(self, record):
@@ -29,6 +33,11 @@ def apply(logger=logging.getLogger()):
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
 
-    ch.setFormatter(CustomFormatter())
+    ch.setFormatter(ColorfulFormatter())
 
     logger.addHandler(ch)
+
+
+def printc(col, *args, **kwargs):
+    msg = col + ' '.join(map(str, args)) + RESET
+    print(msg, **kwargs)
