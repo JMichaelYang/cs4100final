4ee98f4344688139297d7312552b272d5a994ab7
camplowell <camplowell@gmail.com>
2021-12-10 12:41:41 -0500

    Add options, increase verbosity

--- training_test.py
+++ training_test.py
@@ -1,7 +1,25 @@
 import lstm.music_training as training
+import sys
+import logging
+import util.coloredLogging as cl
+
+logging.getLogger().setLevel(logging.INFO)
+cl.init_colors()
+
+if '--help' in sys.argv:
+    print('Tests the training code with way too few epochs')
+    print('Arguments:')
+    print('  --no-save')
+    print('  --play')
+    exit(0)
 
 model = training.trainModel(10)
 model.zero_grad()
 
 print('\nwriting songs...')
-training.runModel(model, 3)
+outputs = training.runModel(model, 3, '--no-save' not in sys.argv)
+
+if '--play' in sys.argv:
+    import expressive.player as player
+    for song in outputs:
+        player.play(song)
